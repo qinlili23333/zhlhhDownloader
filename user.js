38103c317c10600de9f03ee68d251a9325a30cfb
琴梨梨 <yeyaohua2010@163.com>
2021-09-02 20:25:47 +0800

    SakiProgress,Yes!

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         连环画下载器
 // @namespace    https://qinlili.bid/
-// @version      0.1
+// @version      0.2
 // @description  自动打包下载
 // @author       琴梨梨
 // @match        http://wx.zhlhh.com/Reader/*?*
@@ -26,23 +26,24 @@
         document.querySelector("body").innerHTML="<H2>打包需要时间，请耐心等待</H2><H2>本页面仅用于CORS注入，分享网址没有用</H2>"
         var searchParams = new URLSearchParams(document.location.search);
         imgInfo = JSON.parse(atob(searchParams.get("json") ))
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://cdn.jsdelivr.net/npm/jszip@3.6.0/dist/jszip.min.js";
-        document.body.appendChild(script);
         document.title="下载：" + searchParams.get("title");
         var zip;
         function makeZIP(){
-            downloadBtn.innerText = "正在下载..."
+            SakiProgress.showDiv();
+            SakiProgress.setText("准备下载...");
+            downloadBtn.innerText="工作中...";
+            downloadBtn.disabled=true;
             var i = 0;
             zip = new JSZip();
             function dlNext() {
-                downloadBtn.innerText = "正在下载...已处理" + i + "页";
+                SakiProgress.setText("正在处理" + (i+1) + "页...");
+                SakiProgress.setPercent(i/imgInfo.length*80);
                 fetch(imgInfo[i].src).then(res => res.blob().then(blob => {
                     zip.file((i + 1) + ".jpg", blob, { binary: true })
                     i = i + 1;
                     if (imgInfo[i]) { dlNext(); }else{
-                        downloadBtn.innerText = "正在打包..."
+                        SakiProgress.setText( "正在打包...");
+                        SakiProgress.setPercent(80);
                         dlZIP();
                     }
                 }))
@@ -50,20 +51,45 @@
             dlNext();
         }
         var downloadBtn = document.createElement("button");
-        downloadBtn.innerText = "打包下载";
+        downloadBtn.innerText = "正在初始化...";
         downloadBtn.onclick = function () { makeZIP() }
+        downloadBtn.disabled=true;
         document.querySelector("body").appendChild(downloadBtn);
         function dlZIP(){
             zip.generateAsync({ type: "blob" }).then(function (blob) {
+                SakiProgress.setText( "正在导出...");
+                SakiProgress.setPercent(90);
                 var a = document.createElement('a');
                 var url = window.URL.createObjectURL(blob);
-                var filename = searchParams.get("title")  + ".zip";
+                var filename = searchParams.get("title") + ".zip";
                 a.href = url;
                 a.download = filename;
                 a.click();
                 window.URL.revokeObjectURL(url);
                 downloadBtn.innerText="保存成功！";
+                setTimeout(function(){
+                    SakiProgress.setText( "下载成功！");
+                    SakiProgress.setPercent(100);
+                    SakiProgress.clearProgress();
+                },1000)
             });
+        }
+        downloadBtn.innerText = "正在加载JSZip依赖...";
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://cdn.jsdelivr.net/npm/jszip@3.6.0/dist/jszip.min.js";
+        document.body.appendChild(script);
+        script.onload=function(){
+            downloadBtn.innerText = "正在加载SakiProgress依赖...";
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "https://cdn.jsdelivr.net/gh/qinlili23333/SakiProgress@1.0.3/SakiProgress.min.js";
+            document.body.appendChild(script);
+            script.onload=function(){
+                downloadBtn.innerText = "打包下载";
+                SakiProgress.init();
+                downloadBtn.disabled=false;
+            }
         }
     }
 })();
